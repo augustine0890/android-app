35cde0c6447cc8c96d7d1b400fe1b6748ad51482
augustine <augustino0890@gmail.com>
2020-08-17 22:51:49 +0900

    Datatypes of numbers

--- android10_kotlin_dev/android_studio/app/src/main/java/com/example/helloworld/KotlinBasics.kt
+++ android10_kotlin_dev/android_studio/app/src/main/java/com/example/helloworld/KotlinBasics.kt
@@ -1,8 +1,26 @@
 package com.example.helloworld
 
 fun main(){
-    var myName = "Augustine"
+    var myName = "Augustine" // mutable variable
     myName = "Quy"
-    val myFamilyName = "Nguyen"
+    val myFamilyName = "Nguyen" // immutable variable
+    var myAge = 30
     print("Hello $myName $myFamilyName")
+
+    /*
+    This is multiple comment
+    Integer TYPES: Byte (8 bit), Short (16 bit)
+    Int (32 bit), Long (64 bit)
+     */
+    val myByte: Byte = 15
+    val myShort: Short = 125
+    val myInt: Int = 1234123412
+    val myLong: Long = 12_039_812_487_120_39
+
+    // Floating Point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 15.23F
+    val myDouble: Double = 3.14159265358979323846
+
+
+
 }
