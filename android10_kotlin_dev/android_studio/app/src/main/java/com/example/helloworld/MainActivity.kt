96de687024adfcdb7358d18728b7980302b7448b
augustine <augustino0890@gmail.com>
2020-08-17 22:08:11 +0900

    Optimize android code

--- android10_kotlin_dev/android_studio/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ android10_kotlin_dev/android_studio/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // get reference to button
-        val btn_click_me = findViewById(R.id.button) as Button
-        var myTextView = findViewById(R.id.textView) as TextView
+        // val btnClickMe = findViewById(R.id.button) as Button
+        // var myTextView = findViewById(R.id.textView) as TextView
         var timesClicked = 0
         // set on-click listener
-        btn_click_me.setOnClickListener {
-            timesClicked = timesClicked + 1
-            myTextView.text = timesClicked.toString()
+        button.setOnClickListener {
+            timesClicked += 1
+            textView.text = timesClicked.toString()
             Toast.makeText(this@MainActivity,"Hi! Augustine.", Toast.LENGTH_SHORT).show()
 
         }
