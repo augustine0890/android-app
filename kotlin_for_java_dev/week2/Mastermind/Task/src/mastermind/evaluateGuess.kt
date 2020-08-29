2a610f02b7c786957c2a1ceb2fd1ccffbe469594
augustine <augustino0890@gmail.com>
2020-08-30 00:39:36 +0900

    Mastermind game

--- kotlin_for_java_dev/week2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ kotlin_for_java_dev/week2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,7 +1,13 @@
 package mastermind
+import kotlin.math.min
 
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    TODO()
+ val rightPosition = secret.zip(guess).count { it.first == it.second }
+ val wrongPosition = guess.toSet().sumBy { ch ->
+  min(secret.count { it == ch }, guess.count { it == ch })
+ } - rightPosition
+
+ return Evaluation(rightPosition, wrongPosition)
 }
