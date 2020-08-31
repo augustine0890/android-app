5c27660856bde96650c45dd27822922b8fb266d0
augustine <augustino0890@gmail.com>
2020-09-01 00:15:49 +0900

    Assignment week3

--- kotlin_for_java_dev/week3/Nice String/Task/src/nicestring/NiceString.kt
+++ kotlin_for_java_dev/week3/Nice String/Task/src/nicestring/NiceString.kt
@@ -1,5 +1,7 @@
 package nicestring
 
-fun String.isNice(): Boolean {
-    TODO()
-}
+fun String.isNice(): Boolean = listOf(
+        setOf("bu", "ba", "be").none { it in this },
+        count { it in "aeiou" } >= 3,
+        zipWithNext().any { it.first == it.second }
+).count { it } >= 2
